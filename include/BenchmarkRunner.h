0a54e9a1fd92887f8b34377e753299203758812b
William Cameron <gnomorian@gmail.com>
2021-12-20 20:01:52 +1300

    added BenchmarkRunner::run()

--- include/BenchmarkRunner.h
+++ include/BenchmarkRunner.h
@@ -20,9 +20,17 @@ namespace wiwyum::benchmark
 		/// </summary>
 		/// <param name="newTest">the non-owning unique <see cref="wiwyum::benchmark::BenchmarkTest">BenchmarkTest</see></param>
 		/// <returns>a reference to self</returns>
-		BenchmarkRunner& addTest(BenchmarkTest& newTest);
+		virtual BenchmarkRunner& addTest(BenchmarkTest& newTest);
+		/// <summary>
+		/// run all the <see cRef="BenchmarkRunner::tests">tests</see> for the given <see cRef="BenchmarkRunner::NumRuns">number of runs</see>
+		/// </summary>
+		/// <returns>a map containing the timer results for each testid
+		/// //TODO: change TimerResultsArray to a map of string,time_point containing the custom defined benchmark keys for each test incase someone wants total time and sub transaction times for instance.
+		/// </returns>
+		virtual std::map<BenchmarkTest::TestId, TimerResultsArray> run();
 	protected:
+		virtual void assertNumRunsNotZero() const;
+
 		std::set<BenchmarkTest*, BenchmarkTestLessComparitor> tests;
-		std::map<BenchmarkTest::TestId, TimerResultsArray> results;
 	};
 }
